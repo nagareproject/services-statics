1010eea12830d33f158f2a30fb331d55ae80e29a
Alain Poirier <alain.poirier@net-ng.com>
2018-10-26 16:34:19 +0200

    Initial checkin

---
+++ nagare/services/statics.py
@@ -0,0 +1,93 @@
+# --
+# Copyright (c) 2008-2018 Net-ng.
+# All rights reserved.
+#
+# This software is licensed under the BSD License, as described in
+# the file LICENSE.txt, which you should have received as part of
+# this distribution.
+# --
+
+import os
+import mimetypes
+
+from webob import exc, response
+from nagare.services import plugin
+
+
+class Statics(plugin.Plugin):
+    LOAD_PRIORITY = 30
+
+    def __init__(self, name, dist):
+        super(Statics, self).__init__(name, dist)
+        self.routes = []
+
+    def register_static(self, url, dirname):
+        if os.path.isdir(dirname):
+            self.register(url, os.path.abspath(dirname))
+
+    def register(self, url, app=None):
+        url = url.strip('/')
+        url = ('/%s/' % url) if url else '/'
+
+        self.routes.append((url, app))
+        self.routes.sort(key=lambda e: len(e[0]), reverse=True)
+
+    def info(self):
+        super(Statics, self).info()
+
+        print('\nRoutes:\n')
+        for url, dirname in sorted(self.routes):
+            print('  ', url, '->', dirname or '<application>')
+
+    @staticmethod
+    def iter_file(filename, chunk_size=4096):
+        with open(filename, 'rb') as fileobj:
+            chunk = b'.'
+            while chunk:
+                chunk = fileobj.read(chunk_size)
+                if chunk:
+                    yield chunk
+
+    def serve_file(self, dirname, filename):
+        filename = os.path.normpath(os.path.join(dirname, *filename.split('/')))
+        print('Serving', filename)
+
+        if not filename.startswith(dirname + '/') or not os.path.isfile(filename):
+            res = exc.HTTPNotFound()
+        else:
+            mime, encoding = mimetypes.guess_type(filename)
+            mime = mime or 'application/octet-stream'
+
+            size = os.path.getsize(filename)
+            time = os.path.getmtime(filename)
+
+            res = response.Response(
+                content_type=mime,
+                content_length=size,
+                app_iter=self.iter_file(filename),
+                conditional_response=True
+            )
+            res.last_modified = time
+            res.etag = '%s-%s-%s' % (time, size, hash(filename))
+
+        return res
+
+    def handle_request(self, chain, request, **params):
+        path_info = request.path_info.rstrip('/')
+
+        for url, dirname in self.routes:
+            if (path_info + '/').startswith(url):
+
+                if dirname:
+                    response = self.serve_file(dirname, path_info[len(url):])
+                else:
+                    request.script_name = request.script_name.rstrip('/') + url[:-1]
+                    request.path_info = path_info[len(url) - 1:]
+
+                    response = chain.next(request=request, **params)
+
+                break
+        else:
+            response = exc.HTTPNotFound(comment=path_info)
+
+        return response
---
+++ tests/test_path_info.py
@@ -0,0 +1,116 @@
+# --
+# Copyright (c) 2008-2018 Net-ng.
+# All rights reserved.
+#
+# This software is licensed under the BSD License, as described in
+# the file LICENSE.txt, which you should have received as part of
+# this distribution.
+# --
+
+from nagare.services.statics import exc, Statics
+
+
+class Request(object):
+    def __init__(self, path_info, script_name=''):
+        self.path_info = path_info
+        self.script_name = script_name
+
+
+class Chain(object):
+    def next(self, request):
+        return request.path_info, request.script_name
+
+
+def dispatch(urls, path_info, script_name):
+    statics = Statics(None, None)
+
+    if not isinstance(urls, tuple):
+        urls = (urls,)
+
+    for url in urls:
+        statics.register(url)
+
+    return statics.handle_request(Chain(), Request(path_info, script_name))
+
+
+def test_empty():
+    assert dispatch('', '', '') == ('', '')
+    assert dispatch('', '/', '') == ('', '')
+    assert dispatch('/', '', '') == ('', '')
+    assert dispatch('/', '/', '') == ('', '')
+
+    assert dispatch('', '/x', '') == ('/x', '')
+    assert dispatch('', '/x/', '') == ('/x', '')
+    assert dispatch('/', '/x', '') == ('/x', '')
+    assert dispatch('/', '/x/', '') == ('/x', '')
+
+    assert dispatch('', '', '/y') == ('', '/y')
+    assert dispatch('', '/', '/y/') == ('', '/y')
+    assert dispatch('/', '', '/y') == ('', '/y')
+    assert dispatch('/', '/', '/y/') == ('', '/y')
+
+    assert dispatch('', '/x', '/y') == ('/x', '/y')
+    assert dispatch('', '/x/', '/y/') == ('/x', '/y')
+    assert dispatch('/', '/x', '/y') == ('/x', '/y')
+    assert dispatch('/', '/x/', '/y/') == ('/x', '/y')
+
+
+def test_whole_prefix():
+    assert dispatch('/demo', '/demo', '') == ('', '/demo')
+    assert dispatch('/demo/', '/demo', '') == ('', '/demo')
+    assert dispatch('demo/', '/demo', '') == ('', '/demo')
+    assert dispatch('demo', '/demo', '') == ('', '/demo')
+
+    assert dispatch('/demo', '/demo/', '') == ('', '/demo')
+    assert dispatch('/demo/', '/demo/', '') == ('', '/demo')
+    assert dispatch('demo/', '/demo/', '') == ('', '/demo')
+    assert dispatch('demo', '/demo/', '') == ('', '/demo')
+
+    assert dispatch('/demo', '/demo', '/y') == ('', '/y/demo')
+    assert dispatch('/demo/', '/demo', '/y/') == ('', '/y/demo')
+    assert dispatch('demo/', '/demo', '/y') == ('', '/y/demo')
+    assert dispatch('demo', '/demo', '/y/') == ('', '/y/demo')
+
+
+def test_prefix():
+    assert dispatch('/demo', '/demo/x', '') == ('/x', '/demo')
+    assert dispatch('/demo/', '/demo/x', '') == ('/x', '/demo')
+    assert dispatch('demo/', '/demo/x', '') == ('/x', '/demo')
+    assert dispatch('demo', '/demo/x', '') == ('/x', '/demo')
+
+    assert dispatch('/demo', '/demo/x/', '') == ('/x', '/demo')
+    assert dispatch('/demo/', '/demo/x/', '') == ('/x', '/demo')
+    assert dispatch('demo/', '/demo/x/', '') == ('/x', '/demo')
+    assert dispatch('demo', '/demo/x/', '') == ('/x', '/demo')
+
+    assert dispatch('/demo', '/demo/x', '/y') == ('/x', '/y/demo')
+    assert dispatch('/demo/', '/demo/x', '/y') == ('/x', '/y/demo')
+    assert dispatch('demo/', '/demo/x', '/y') == ('/x', '/y/demo')
+    assert dispatch('demo', '/demo/x', '/y') == ('/x', '/y/demo')
+
+    assert dispatch('/demo', '/demo/x/', '/y') == ('/x', '/y/demo')
+    assert dispatch('/demo/', '/demo/x/', '/y') == ('/x', '/y/demo')
+    assert dispatch('demo/', '/demo/x/', '/y') == ('/x', '/y/demo')
+    assert dispatch('demo', '/demo/x/', '/y') == ('/x', '/y/demo')
+
+
+def test_not_found():
+    assert dispatch('', '', '') == ('', '')
+    assert dispatch('', '/x', '') == ('/x', '')
+
+    assert isinstance(dispatch('/demo', '', ''), exc.HTTPNotFound)
+    assert isinstance(dispatch('/demo', '/x', ''), exc.HTTPNotFound)
+
+
+def test_sorted():
+    assert dispatch(('/demo', '/demoo'), '/demo', '') == ('', '/demo')
+    assert dispatch(('/demo', '/demoo'), '/demo/x/y', '') == ('/x/y', '/demo')
+
+    assert dispatch(('/demoo', '/demo'), '/demo', '') == ('', '/demo')
+    assert dispatch(('/demoo', '/demo'), '/demo/x/y', '') == ('/x/y', '/demo')
+
+    assert dispatch(('/demo', '/demoo'), '/demoo', '') == ('', '/demoo')
+    assert dispatch(('/demo', '/demoo'), '/demoo/x/y', '') == ('/x/y', '/demoo')
+
+    assert dispatch(('/demoo', '/demo'), '/demoo', '') == ('', '/demoo')
+    assert dispatch(('/demoo', '/demo'), '/demoo/x/y', '') == ('/x/y', '/demoo')
