b5c14dc2dfcaa01a2abd3bd85172ff7d6e8a6256
Alain Poirier <alain.poirier@net-ng.com>
2021-07-22 17:14:16 +0200

    Command `nagare-admin mountpoints <config>` added

---
+++ nagare/admin/statics_commands.py
@@ -0,0 +1,31 @@
+# --
+# Copyright (c) 2008-2021 Net-ng.
+# All rights reserved.
+#
+# This software is licensed under the BSD License, as described in
+# the file LICENSE.txt, which you should have received as part of
+# this distribution.
+# --
+
+from nagare.admin import command
+
+
+class Mountpoints(command.Command):
+    WITH_STARTED_SERVICES = True
+    DESC = 'display URL mountpoints'
+
+    @staticmethod
+    def run(statics_service):
+        mountpoints = statics_service.mountpoints
+
+        print('Mountpoints')
+        print('-----------')
+        print()
+
+        if not mountpoints:
+            print('<empty>')
+        else:
+            for url, handler in mountpoints:
+                print('{} -> {}'.format(url, handler))
+
+        return 0
