877e69b36df556810191b4c1664fbcda8a20dd5d
Alain Poirier <alain.poirier@net-ng.com>
2022-02-25 18:36:28 +0100

    `-u` / `--url` option added to the `nagare-admin mountpoints` command

--- nagare/admin/statics_commands.py
+++ nagare/admin/statics_commands.py
@@ -14,18 +14,34 @@ class Mountpoints(command.Command):
     WITH_STARTED_SERVICES = True
     DESC = 'display URL mountpoints'
 
+    def set_arguments(self, parser):
+        parser.add_argument(
+            '-u', '--url',
+            action='append', dest='urls', metavar='URL',
+            help='display the mountpoints for the given URLs'
+        )
+
+        super(Mountpoints, self).set_arguments(parser)
+
     @staticmethod
-    def run(statics_service):
+    def run(statics_service, urls):
+        urls = {url: i for i, url in enumerate(urls or [])}
+
         mountpoints = statics_service.mountpoints
 
-        print('Mountpoints')
-        print('-----------')
-        print()
+        if not urls:
+            print('Mountpoints')
+            print('-----------')
+            print()
 
-        if not mountpoints:
-            print('<empty>')
-        else:
-            for url, handler in mountpoints:
-                print('{} -> {}'.format(url, handler))
+            if not mountpoints:
+                print('<empty>')
+
+        if mountpoints:
+            url_maxlen = max(len(url) for url, _ in mountpoints)
+
+            mountpoints = [e for e in mountpoints if not urls or (e[0] in urls)]
+            for url, handler in sorted(mountpoints, key=lambda e: urls.get(e[0], 0)):
+                print(('{2}' if urls else '{:{}} -> {}').format(url, url_maxlen, handler))
 
         return 0
